45063b9f0b9a67a2e529fe654fe22451249f7a33
Subhash Kumar <jietss2011@gmail.com>
2016-09-15 12:21:58 +0530

    initialize repository

---
+++ db/migrate/20160822121702_create_contacts.rb
@@ -0,0 +1,22 @@
+class CreateContacts < ActiveRecord::Migration
+ # def change
+  #  create_table :contacts do |t|
+
+   ##end
+   def up
+		create_table :contacts do |t|
+
+		t.string :mobile , :null => false, :default => ""
+		t.string :address , :null => false, :default => ""
+
+		end
+
+	end
+
+	def down
+		drop_table :contacts
+
+	end
+
+  
+end
